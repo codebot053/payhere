11351b1a4e8a0fd7d3ef634cc0df7635a4d4a6db
codebot053 <codebot053@gmail.com>
2023-01-16 01:42:46 +0900

    Feat: user/models.py has_perm, has_module_perms 추가 User 모델 admin page 등록을 위한 메서드 추가

--- user/models.py
+++ user/models.py
@@ -45,8 +45,15 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return f"eamil: {self.email} / name: {self.name}"
-        
+
+    def has_perm(self, perm, obj=None):
+       return self.is_admin
+
+    def has_module_perms(self, app_label):
+       return self.is_admin
+
     @property
     def is_staff(self):
         return self.is_admin
 
+    
