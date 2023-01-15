500e1d798394917e58a6fbd6c01ae0363d93806e
codebot053 <codebot053@gmail.com>
2023-01-15 23:46:26 +0900

    Feat: moneybook/models.py MoneyBook table 추가

--- moneybook/models.py
+++ moneybook/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class MoneyBook(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    cash_amount = models.IntegerField(default=0)
+    latest_log_id = models.IntegerField(default=0)
