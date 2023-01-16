305e7d7cb7bb0d71542f94be4184296373617f75
codebot053 <codebot053@gmail.com>
2023-01-16 19:32:03 +0900

    Feat: moneybook/urls.py detali_log path 추가

--- moneybook/urls.py
+++ moneybook/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.MoneyBookLogAPIView.as_view(), name="moneybook_log"),
+    path("<int:log_id>/", views.MoneyBookLogDetailAPIView.as_view(), name="detail_log"),
 ]
