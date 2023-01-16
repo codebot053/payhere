98dd141e3fc8f05c5336daaadd828b9011042589
codebot053 <codebot053@gmail.com>
2023-01-16 19:17:51 +0900

    Feat: moneybook/views.py MoneyBookLogAPIView 추가

--- moneybook/views.py
+++ moneybook/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import MoneyBook, MoneyBookLog
+from .serializers import MoneyBookSerializer
+from .serializers import MoneyBookLogSerializer, MoneyBookLogListSerializer
 
-# Create your views here.
+
+class MoneyBookLogAPIView(APIView):
+    """
+    MoneyBookLog 모델 LIST, CREATE APIView
+    URI: {service_root}/moneybook/
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_id = request.user.id
+        user_moneybook = MoneyBook.objects.get(user=user_id)
+        user_moneybook_log = MoneyBookLog.objects.filter(moneybook=user_moneybook.id)
+
+        return Response(
+            {
+                "userMoneybook": MoneyBookSerializer(user_moneybook).data,
+                "logList": MoneyBookLogListSerializer(
+                    user_moneybook_log, many=True
+                ).data,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def post(self, request):
+        user_id = request.user.id
+        user_moneybook = MoneyBook.objects.get(user=user_id)
+        log_id = user_moneybook.latest_log_id + 1
+        request_data = request.data
+        request_data["log_id"] = log_id
+        request_data["moneybook"] = user_moneybook.id
+        serializer = MoneyBookLogSerializer(data=request_data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user_moneybook.latest_log_id += 1
+            user_moneybook.cash_amount += serializer.data.get("cash")
+            user_moneybook.save()
+
+            return Response({"msg": serializer.data}, status=status.HTTP_201_CREATED)
+
+        return Response(
+            {"error": serializer.error_messages}, status=status.HTTP_400_BAD_REQUEST
+        )
