56347ff3b3033391ac7a9ca4e1317b2f86c12c42
codebot053 <codebot053@gmail.com>
2023-01-16 15:16:33 +0900

    Feat: user/views.py LogInAPIView 추가 유저 로그인시 access, refresh 토큰을 같이 전송

--- user/views.py
+++ user/views.py
@@ -1,7 +1,44 @@
+from django.contrib.auth import authenticate
+from rest_framework import status
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
-from .serializers import UserCreateSerializer
+from .serializers import UserCreateSerializer, UserLogInSerializer
+
 
 class SignUpView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+
+
+class LogInAPIView(APIView):
+    """
+    유저 로그인
+    {
+        "email": "example@test.com",
+        "password": "example"
+    }
+    """
+
+    def post(self, request):
+
+        user = authenticate(
+            email=request.data.get("email"), password=request.data.get("password")
+        )
+        if user is not None:
+            serializer = UserLogInSerializer(user)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            return Response(
+                {
+                    "user": serializer.data,
+                    "message": "로그인 성공",
+                    "access": access_token,
+                    "refresh": refresh_token,
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response({"message": "로그인 실패"}, status=status.HTTP_400_BAD_REQUEST)
