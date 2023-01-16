90e4427f9374aa7643b6d6472ffc7f0d142ef99c
codebot053 <codebot053@gmail.com>
2023-01-16 19:05:27 +0900

    Feat: moneybook/serializers.py ModelSerializer 추가

---
+++ moneybook/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import MoneyBook, MoneyBookLog
+
+
+class MoneybookSerializer(serializers.ModelSerializer):
+    """
+    model: MoneyBook
+    LIST 사용
+    """
+
+    class Meta:
+        model = MoneyBook
+        fields = ["cash_amount"]
+
+
+class MoneyBookLogSerializer(serializers.ModelSerializer):
+    """
+    model: MoneyBookLog
+    CREATE, RETRIEVE 사용
+    """
+
+    class Meta:
+        model = MoneyBookLog
+        fields = "__all__"
+
+
+class MoneyBookLogListSerializer(serializers.ModelSerializer):
+    """
+    model: MoneyBookLog
+    LIST 사용
+    """
+
+    class Meta:
+        model = MoneyBookLog
+        fields = ["log_id", "log_status", "cash", "created_at"]
